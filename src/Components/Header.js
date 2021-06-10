7aac51eab3d02b1a001c24ddf9b667b459f2d1cf
nwlee7950 <52395200+nwlee7950@users.noreply.github.com>
2021-06-10 23:43:48 +0900

    header 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router'
+import React from "react";
+import styled from "styled-components";
+import {Link} from 'react-router-dom';
+import Login from "../Routes/Login";
 
-class Header extends Component {
-  render() {
+// const listStyle = styled.li`   list-style: none;   align-items: center; `;
+const HeaderSpace = styled.header `
+width:100%
+height: 80px;
+display: flex;
+background-color: #bdc3c7;
+`;
+
+const HeaderLogo = styled.img`
+justify-content: center;
+`;
+
+const Header = () => {
     return (
-      <div className="flex pa1 justify-between nowrap orange">
-        <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
-            home
-          </Link>
-          <div className="ml1">|</div>
-          <Link to="/Login" className="ml1 no-underline black">
-            Login
-          </Link>
-        </div>
-      </div>
-    )
-  }
-}
+      <HeaderSpace>
+        <ul id="header_contents">
+            <Link to="/login">BEST 30</Link>
+            <Link to="/login">SALE</Link>
+            <Link to="/login">SHOP</Link>
+            <Link to="/login">LOOKBOOK</Link>
+        </ul>
+      </HeaderSpace>
+    );
+};
 
-export default withRouter(Header)
+export default Header;
--- src/Routes/Home/HomeView.js
+++ src/Routes/Home/HomeView.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Header from "../../Components/Header";
 
 const HomeView = () => {
     return (
         <div>
+            <Header />
             홈
             <button onClick={() => {window.location.href = '/login'}}>
                 버어튼
