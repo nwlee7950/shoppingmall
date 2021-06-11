9afbeedb2a122aa68456ff535f29410990090086
nwlee7950 <52395200+nwlee7950@users.noreply.github.com>
2021-06-11 15:33:23 +0900

    header 수정

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,28 +2,62 @@ import React from "react";
 import styled from "styled-components";
 import {Link} from 'react-router-dom';
 import Login from "../Routes/Login";
+import Logo from "../Images/Logo.png";
 
-// const listStyle = styled.li`   list-style: none;   align-items: center; `;
 const HeaderSpace = styled.header `
 width:100%
 height: 80px;
 display: flex;
-background-color: #bdc3c7;
+border-bottom: solid;
+justify-content: space-between;
 `;
 
 const HeaderLogo = styled.img`
-justify-content: center;
+width: 170px;
+text-align: center;
+`;
+
+const List = styled.ul`
+list-style: none;
+background-color: black;
+`;
+
+const ListItem = styled.li`
+background-color: green;
+float: left;
+margin-right: 20px;
 `;
 
 const Header = () => {
     return (
       <HeaderSpace>
-        <ul id="header_contents">
+        <List>
+          <ListItem>
             <Link to="/login">BEST 30</Link>
+          </ListItem>
+          <ListItem>
             <Link to="/login">SALE</Link>
+          </ListItem>
+          <ListItem>
             <Link to="/login">SHOP</Link>
+          </ListItem>
+          <ListItem>
             <Link to="/login">LOOKBOOK</Link>
-        </ul>
+          </ListItem>
+        </List>
+        <HeaderLogo src={Logo}></HeaderLogo>
+        <List>
+        <ListItem>
+            <Link to="/login">COMMUNITY</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/login">ACCOUNT</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/login">CART</Link>
+          </ListItem>
+          
+        </List>
       </HeaderSpace>
     );
 };
