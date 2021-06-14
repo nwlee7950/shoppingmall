9b617629e9b22e1c3c0e1582331883bc1f21fa08
nwlee7950 <52395200+nwlee7950@users.noreply.github.com>
2021-06-14 23:44:48 +0900

    dropdown 수정중

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Dropdown from "react-dropdown";
 import { Link } from "react-router-dom";
 import Login from "../Routes/Login";
 import Register from "../Routes/Register";
@@ -28,6 +29,27 @@ const ListItem = styled.li`
   float: left;
   margin-right: 20px;
 `;
+
+// const options = [
+//   { value: "one", label: "One" },
+//   { value: "two", label: "Two", className: "myOptionClassName" },
+//   {
+//     type: "group",
+//     name: "group1",
+//     items: [
+//       { value: "three", label: "Three", className: "myOptionClassName" },
+//       { value: "four", label: "Four" },
+//     ],
+//   },
+//   {
+//     type: "group",
+//     name: "group2",
+//     items: [
+//       { value: "five", label: "Five" },
+//       { value: "six", label: "Six" },
+//     ],
+//   },
+// ];
 
 const Header = () => {
   return (
