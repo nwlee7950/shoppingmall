dfb27173e4681542d7767e13932fa689dd23ddb1
nwlee7950 <52395200+nwlee7950@users.noreply.github.com>
2021-06-25 22:22:31 +0900

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -40,27 +40,6 @@ const ListItem = styled.li`
   text-decoration: none;
 `;
 
-// const options = [
-//   { value: "one", label: "One" },
-//   { value: "two", label: "Two", className: "myOptionClassName" },
-//   {
-//     type: "group",
-//     name: "group1",
-//     items: [
-//       { value: "three", label: "Three", className: "myOptionClassName" },
-//       { value: "four", label: "Four" },
-//     ],
-//   },
-//   {
-//     type: "group",
-//     name: "group2",
-//     items: [
-//       { value: "five", label: "Five" },
-//       { value: "six", label: "Six" },
-//     ],
-//   },
-// ];
-
 const Header = () => {
   return (
     <HeaderSpace>
