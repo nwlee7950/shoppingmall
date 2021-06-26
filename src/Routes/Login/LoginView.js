676e061de25fec02fa92a6060acfffe38430f27a
nwlee7950 <52395200+nwlee7950@users.noreply.github.com>
2021-06-26 23:44:40 +0900

    Update LoginView.js

--- src/Routes/Login/LoginView.js
+++ src/Routes/Login/LoginView.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Header from "../../Components/Header";
+import Contents, { ContentsSpace } from "../../Components/Contents";
+
+const InputField = styled.input`
+  width: 400px;
+  height: 40px;
+`;
+
+const LoginButton = styled.button`
+  width: 400px;
+  height: 30px;
+`;
 
 const LoginView = () => {
-    return (
-        <div>
-            로그인
-            <button onClick={() => {window.location.href = '/'}}>
-                버어튼
-            </button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Header></Header>
+      <ContentsSpace>
+        <InputField type="text" name="id" placeholder="아이디"></InputField>
+        <InputField
+          type="text"
+          name="password"
+          placeholder="비밀번호"
+        ></InputField>
+        <LoginButton>로그인</LoginButton>
+      </ContentsSpace>
+    </div>
+  );
+};
 export default LoginView;
